0763ada942da4cb84b9abb0c25cb729fb0c8674a
Karmila11 <36057919+Karmila11@users.noreply.github.com>
2018-02-02 11:24:39 +0800

    v1

---
+++ app/build/generated/source/buildConfig/debug/com/benoitletondor/easybudgetapp/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.benoitletondor.easybudgetapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.benoitletondor.easybudgetapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "1.5.2";
+  // Fields from build type: debug
+  public static final boolean ANALYTICS_ACTIVATED = false;
+  public static final String BATCH_API_KEY = "BATCH_DEV_API_KEY";
+  public static final boolean CRASHLYTICS_ACTIVATED = false;
+  public static final boolean DEBUG_LOG = true;
+  public static final boolean DEV_PREFERENCES = true;
+  public static final String GCM_SENDER_ID = "YOUR_GCM_SENDER_ID";
+  public static final String LICENCE_KEY = "YOUR_GOOGLE_PLAY_LICENCE_KEY";
+}
